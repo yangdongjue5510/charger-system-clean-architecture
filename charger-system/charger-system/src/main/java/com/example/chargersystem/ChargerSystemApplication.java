7545da5c5e612f861d176479b32e9e37998fb425
1128016 <djwhy5510@hyundai-autoever.com>
2024-03-15 10:09:28 +0900

    init project

---
+++ charger-system/charger-system/src/main/java/com/example/chargersystem/ChargerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.chargersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargerSystemApplication.class, args);
+	}
+
+}
---
+++ charger-system/charger-system/src/test/java/com/example/chargersystem/ChargerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chargersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
